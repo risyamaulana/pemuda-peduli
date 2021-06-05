0183cb1285047f78e337d9469ecb29ec3c268426
Risya <risyamaulana25@gmail.com>
2021-06-05 22:35:03 +0700

    Update 5 June 2021

--- src/program_donasi/applications/program_donasi_payload.go
+++ src/program_donasi/applications/program_donasi_payload.go
@@ -9,8 +9,8 @@ import (
 )
 
 type CreateProgramDonasi struct {
-	Title             string     `json:"title" valid:"required"`
-	SubTitle          string     `json:"sub_title" valid:"required"`
+	Title             string     `json:"title" valid:"required,alphanum"`
+	SubTitle          string     `json:"sub_title" valid:"required,alphanum"`
 	Tag               string     `json:"tag"`
 	ThumbnailImageURL string     `json:"thumbnail_image_url" valid:"url"`
 	ValidFrom         *time.Time `json:"valid_from" valid:"required"`
